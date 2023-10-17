45440457fb1a0ea6b9ba7b0f8a5c796271878ce9
Electroshockist <ericonfire2@yahoo.com>
2023-10-16 20:19:27 -0400

    Updated player controller
    Added camera clamp
    Removed gimbal (unnecessary)
    Fixed camera orientation

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,7 +2,8 @@ extends CharacterBody3D
 
 #Camera controls
 @export var rotation_sensitvity: float = 4
-@export var camera_clamps: Vector2 = Vector2(30, -85)
+@export var min_pitch: = -30
+@export var max_pitch = 85
 
 @export var speed : float = 10
 @export var jump_force : float = 150
@@ -13,23 +14,24 @@ const GRAVITY = -9.81
 var horizontal : Input_Pair
 var vertical : Input_Pair
 
-var gimbal:Node3D
-
 var input_dir: Vector2 = Vector2.ZERO
 
 func _ready():
 	horizontal = $Horizontal
 	vertical = $Vertical
-	gimbal = $Gimbal
 
 func _input(event):	
-	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:		
-		self.rotate_y(-event.relative.x * rotation_sensitvity * 0.001)
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		var rot: Vector2 = Vector2(event.relative.y * rotation_sensitvity * 0.001, -event.relative.x * rotation_sensitvity * 0.001)
 		
-		#print(gimbal.rotation.x)
-		#gimbal.rotation.x = clampf(gimbal.rotation.x + event.relative.y * rotation_sensitvity * 0.001, camera_clamps.y, camera_clamps.x)
-		gimbal.rotate_x(event.relative.y * rotation_sensitvity * 0.001)
-		print(gimbal.get)
+		$Camera3D.rotation.x = clamp($Camera3D.rotation.x - rot.x, deg_to_rad(min_pitch), deg_to_rad(max_pitch))
+		print(rad_to_deg($Camera3D.rotation.x))
+		
+		#var can_turn : bool = not(camrot >= deg_to_rad(max_pitch) and rot.x > 0 or camrot <= deg_to_rad(min_pitch) and rot.x <0)
+		
+		#if can_turn:
+			#$Camera3D.rotate_x(rot.x)
+		rotate_y(rot.y)
 
 func _physics_process(_delta):
 	input_dir = Vector2(
@@ -38,7 +40,7 @@ func _physics_process(_delta):
 		)
 	input_dir = input_dir.normalized() if input_dir.length() > 1 else  input_dir
 	
-	var final_input = Vector3(input_dir.x, 0, input_dir.y) * speed * transform.basis.inverse()
+	var final_input = -Vector3(input_dir.x, 0, input_dir.y) * speed * transform.basis.inverse()
 	
 	velocity = final_input + Vector3(0, velocity.y ,0)
 	#print(is_on_floor())
